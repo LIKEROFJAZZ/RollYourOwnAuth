e4c872b28dd7f1bb020246f66f447da8d3cf6aa7
LIKEROFJAZZ <luc.lachance@sait.edu.ca>
2022-02-22 19:50:16 -0700

    API update

--- api/flask_api.py
+++ api/flask_api.py
@@ -1,34 +1,42 @@
-from flask import Flask
 import flask_restful
-from flask_restful import Resource
-from flask_cors import CORS, cross_origin
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 import re
-import random
-import ssl
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 api = flask_restful.Api(app)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-def email_check(email):
+
+def isemail(email):
     regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
     if re.search(regex, email):
         return True
     else:
         return False
 
-class Email(Resource):
+
+flag = "TESTFLAG"
+
+class signup(Resource):
     def post(self):
-        return {'message': "posted successfully"}, 200
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', required=True, help="This is not a valid input. please enter an email address.")
+        input = str(parser.parse_args())
+        #check if input is a valid email address
+        if isemail(input):
+            if "@jazzrules.ca" in input:
 
-    pass
+                return flag, 200
+            else:
+                return "Please use a '@jazzrules.ca email address.", 400
+        else:
+            return "This is not a valid input. please enter an email address.", 400
 
 
-api.add_resource(Email, '/email')
+api.add_resource(signup, '/signup')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8001)
+    app.run(host="127.0.0.1", port="8001")
